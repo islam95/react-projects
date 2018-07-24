c0e8559aae60705613ea08dd5602d1291ec4d2ed
Islam Dudaev <islamwolf23@gmail.com>
2018-07-24 23:35:38 +0100

    set up the clock with time until the deadline

---
+++ src/components/Countdown/Clock.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Countdown.css";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
+  // The time left until the passed date
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    this.setState({ days, hours, minutes, seconds });
+  }
+
+  render() {
+    return (
+      <div>
+        <span className="marginRight">{this.state.days} days</span>
+        <span className="marginRight">{this.state.hours} hours</span>
+        <span className="marginRight">{this.state.minutes} minutes</span>
+        <span className="marginRight">{this.state.seconds} seconds</span>
+      </div>
+    );
+  }
+}
+
+export default Clock;
--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,20 +1,34 @@
 import React from "react";
+import Clock from "./Clock"
+import "./Countdown.css";
 
 class Countdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      deadline: "December 18, 2018",
+      newDeadline: ""
+    };
   }
+
+  changeDeadline() {
+    this.setState({
+      deadline: this.state.newDeadline
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Countdown Component</h1>
-        <h4>Countdown to December 18, 2018</h4>
+        <h4>Countdown to {this.state.deadline}</h4>
+        <Clock deadline={this.state.deadline} />
         <div>
-          <span>14 days</span>
-          <span>14 days</span>
-          <span>14 days</span>
-          <span>14 days</span>
+          <input
+            placeholder="New date"
+            onChange={e => this.setState({ newDeadline: e.target.value })}
+          />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     );
