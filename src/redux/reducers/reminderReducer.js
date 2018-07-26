1bf60182f9022178636fa12559837ca4a690b66b
Islam Dudaev <islamwolf23@gmail.com>
2018-07-26 10:24:52 +0100

    create delete Reminder reducer

--- src/redux/reducers/reminderReducer.js
+++ src/redux/reducers/reminderReducer.js
@@ -1,9 +1,13 @@
-import { ADD_REMINDER } from "../actions/types";
+import { ADD_REMINDER, DELETE_REMINDER } from "../actions/types";
 
 const Reminders = (state = [], action) => {
   switch (action.type) {
     case ADD_REMINDER:
       return [...state, { text: action.text, id: Math.random() }];
+
+    case DELETE_REMINDER:
+      const newState = [...state];
+      return newState.filter(reminder => reminder.id !== action.id)
 
     default:
       return state;
